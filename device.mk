625e59812183b329f05c36a440b38f46aeac5e30
cm <cesar.maximo@gmail.com>
2016-12-26 16:52:34 +0000

    consumerir: remove permission useless...

--- device.mk
+++ device.mk
@@ -66,8 +66,7 @@ PRODUCT_PACKAGES += \
     fingerprintd
 
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml \
-    frameworks/native/data/etc/android.hardware.consumerir.xml:system/etc/permissions/android.hardware.consumerir.xml
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:system/etc/permissions/android.hardware.fingerprint.xml
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
