a595f89cc863317a65c9e813d5eb632157331e76
fire855 <thefire855@gmail.com>
2017-07-02 13:07:32 +0200

    Cleanup

--- device.mk
+++ device.mk
@@ -32,7 +32,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/factory_init.rc:root/factory_init.rc \
     $(LOCAL_PATH)/rootdir/fstab.mt6797:root/fstab.mt6797 \
     $(LOCAL_PATH)/rootdir/init.connectivity.rc:root/init.connectivity.rc \
-    $(LOCAL_PATH)/rootdir/init.mal.rc:root/init.mal.rc \
     $(LOCAL_PATH)/rootdir/init.modem.rc:root/init.modem.rc \
     $(LOCAL_PATH)/rootdir/init.mt6797.rc:root/init.mt6797.rc \
     $(LOCAL_PATH)/rootdir/init.mt6797.usb.rc:root/init.mt6797.usb.rc \
