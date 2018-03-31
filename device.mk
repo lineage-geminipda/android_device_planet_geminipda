081edeb8d7a625c46ea0688747d0d62fcd7c13a4
deadman96385 <seanhoyt963@gmail.com>
2018-03-30 23:49:05 -0500

    Build FM Radio

--- device.mk
+++ device.mk
@@ -104,6 +104,11 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.level-0.xml:system/etc/permissions/android.hardware.vulkan.level.xml \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_0_3.xml:system/etc/permissions/android.hardware.vulkan.version.xml
 
+# FM
+PRODUCT_PACKAGES += \
+    libfmjni \
+    FMRadio
+
 # Recovery allowed devices
 TARGET_OTA_ASSERT_DEVICE := geminipda
 
