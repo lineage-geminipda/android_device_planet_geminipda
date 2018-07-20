b6f9b1ccb97142b33ec73b057963186827fee08c
deadman96385 <seanhoyt963@gmail.com>
2018-07-20 06:13:59 -0500

    Add patches from mt6750

---
+++ patches/install.sh
@@ -0,0 +1,17 @@
+echo $1
+rootdirectory="$PWD"
+# ---------------------------------
+
+dirs="frameworks/av frameworks/base frameworks/native system/core system/netd"
+
+for dir in $dirs ; do
+	cd $rootdirectory
+	cd $dir
+	echo "Applying $dir patches..."
+	git am $rootdirectory/device/mediatek/mt6750-common/patches/$dir/*.patch
+	echo " "
+done
+
+# -----------------------------------
+echo "Changing to build directory..."
+cd $rootdirectory
